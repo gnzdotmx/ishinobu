bb0e30f9ebc66b5bea6dbf5947481cc547cc345c
Javier <gnzdotmx@gmail.com>
2024-10-26 19:55:37 +0900

    Add notificationcenter module

--- ishinobu/go.mod
+++ ishinobu/go.mod
@@ -2,4 +2,7 @@ module github.com/gnzdotmx/ishinobu/ishinobu
 
 go 1.22.1
 
-require github.com/mattn/go-sqlite3 v1.14.24
+require (
+	github.com/mattn/go-sqlite3 v1.14.24
+	howett.net/plist v1.0.1
+)
--- ishinobu/utils/timestamp.go
+++ ishinobu/utils/timestamp.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -18,4 +20,20 @@ func ParseTimestamp(value string) (string, error) {
 	}
 
 	return t.Format(TimeFormat), nil
+}
+
+func ConvertCFAbsoluteTimeToDate(cfTimeStr string) (string, error) {
+	cfTime, err := strconv.ParseFloat(cfTimeStr, 64)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse float from string '%s': %w", cfTimeStr, err)
+	}
+
+	const epochOffset = 978307200
+	unixTimestamp := cfTime + epochOffset
+	seconds := int64(unixTimestamp)
+	nanoseconds := int64((unixTimestamp - float64(seconds)) * 1e9)
+	t := time.Unix(seconds, nanoseconds).UTC()
+	formattedDate := t.Format("2006-01-02 15:04:05")
+
+	return formattedDate, nil
 }
